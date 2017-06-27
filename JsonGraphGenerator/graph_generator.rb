7dfcfdbf067a93c4d629158376265b0a38a7f3ab
CFWLoader <cfwloader@gmail.com>
2017-06-27 11:57:27 +0800

    Checkpoint.

--- JsonGraphGenerator/graph_generator.rb
+++ JsonGraphGenerator/graph_generator.rb
@@ -1,5 +1,6 @@
+require 'set'
 # Graph format: [Source_id, source_value, [[dest_id, edge_value], [dest_id, edge_value]...]]
-class JsonGraphGenerator
+class GraphGenerator
 
   def gen_graph_to_file file_path, **parameters
 
@@ -95,10 +96,49 @@ class JsonGraphGenerator
 
   end
 
+  def gen_edge_list_to_file file_path, node_id_limit, edge_quantity_limit
+
+    save_file = open file_path, 'w'
+
+    gened_edge_count = 0
+
+    1.upto(node_id_limit) { |src_node_id|
+
+      if gened_edge_count >= edge_quantity_limit then break end
+
+      edge_num_to_gen = rand
+
+      tagged_nodes = Set.new
+
+      tagged_nodes.add src_node_id
+
+      1.upto(edge_num_to_gen) {
+
+          if gened_edge_count >= edge_quantity_limit then break end
+
+          tag = rand(node_id_limit)
+
+          unless tagged_nodes.include? tag
+
+            save_file.print "#{src_node_id} #{tag}\n"
+
+            tagged_nodes.add tag
+
+            gened_edge_count += 1
+
+          end
+      }
+
+    }
+
+    save_file.close
+
+  end
+
 end
 
 
-if $0 == __FILE__
+def gen_json_graph
 
   file_path = './test_graph.txt'
 
@@ -106,27 +146,78 @@ if $0 == __FILE__
 
   edge_limit = 10
 
-  unless ARGV[0].nil?
-
-    file_path = ARGV[0]
-
-  end
-
   unless ARGV[1].nil?
 
-    node_limit = ARGV[1].to_i
+    file_path = ARGV[1]
 
   end
 
   unless ARGV[2].nil?
 
-    edge_limit = ARGV[2].to_i
+    node_limit = ARGV[2].to_i
 
   end
 
-  jgg = JsonGraphGenerator.new
+  unless ARGV[3].nil?
+
+    edge_limit = ARGV[3].to_i
+
+  end
+
+  jgg = GraphGenerator.new
 
   #jgg.gen_graph node_limit: 20, edge_limit: 20
   jgg.gen_graph_to_file file_path, node_limit: node_limit, edge_limit: edge_limit
+
+end
+
+
+def gen_edgelist
+
+  file_path = './test_graph.txt'
+
+  node_limit = 100
+
+  edge_limit = 10
+
+  unless ARGV[1].nil?
+
+    file_path = ARGV[1]
+
+  end
+
+  unless ARGV[2].nil?
+
+    node_limit = ARGV[2].to_i
+
+  end
+
+  unless ARGV[3].nil?
+
+    edge_limit = ARGV[3].to_i
+
+  end
+
+  jgg = GraphGenerator.new
+
+  #jgg.gen_graph node_limit: 20, edge_limit: 20
+  jgg.gen_edge_list_to_file file_path, node_limit, edge_limit
+
+end
+
+
+if $0 == __FILE__
+
+  case ARGV[0]
+
+    when 'adjlist' then gen_json_graph
+
+    when 'edgelist' then  gen_edgelist
+
+    else
+
+      print 'Filetype: adjlist, edgelist'
+
+  end
 
 end
