11f258a127b598dd707a36baf27a1e2f32b43273
Evan Huang <cfwloader@gmail.com>
2017-09-13 16:16:10 +0800

    Creating Test Type 02.

---
+++ Test-Type-02/headers/comm/Socket.h
@@ -0,0 +1,26 @@
+//
+// Created by CFWLoader on 9/13/17.
+//
+
+#ifndef TEST_TYPE_02_SOCKET_H
+#define TEST_TYPE_02_SOCKET_H
+
+namespace test_type02
+{
+    namespace comm
+    {
+        class Socket
+        {
+        public:
+
+            Socket();
+
+        private:
+
+            int sock_fd_;
+
+        };
+    }
+}
+
+#endif //TEST_TYPE_02_SOCKET_H
---
+++ Test-Type-02/src/comm/Socket.cpp
@@ -0,0 +1,10 @@
+//
+// Created by CFWLoader on 9/13/17.
+//
+
+#include <comm/Socket.h>
+
+using namespace test_type02::comm;
+
+Socket::Socket()
+{}
